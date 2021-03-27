c19a809dc835d7edcb50c86869537ee0f20ccd5f
Anonymous390 <60838638+Anonymous390@users.noreply.github.com>
2021-03-27 20:10:00 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,39 @@
+import plotly.figure_factory as ff
+from statistics import *
+import random
+import pandas as pd
+
+df = pd.read_csv("test.csv")
+data = df["temperature"].tolist()
+
+population_mean = mean(data)
+
+standard_deviation = stdev(data)
+print(standard_deviation)
+
+def random_set_of_mean(counter):
+    dataset= []
+
+    for i in range(0, counter):
+        random_index = random.randint(0, len(data)-1)
+        value = data[random_index]
+        dataset.append(value)
+
+    sample_mean = mean(dataset)
+    return sample_mean
+
+def setup():
+    mean_list = []
+
+    for i in range(0, 1000):
+        set_of_means = random_set_of_mean(100)
+        mean_list.append(set_of_means)
+
+    plot_graph()
+
+def plot_graph(mean_list):
+    df = mean_list
+    fig = ff.create_distplot([mean_list], ["temperature"])
+    fig.show()
+
+setup()
